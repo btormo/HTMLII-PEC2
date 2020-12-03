62b4bb8b016d9f912558c3d771e2b7d12180e097
btormo <btormo@uoc.edu>
2020-12-03 20:34:12 +0100

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,36 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+import 'bootstrap';
+
++(function () {
+  console.log('Hello, UOC!');
+})();
+
+//Código menú desplegable
+
+function desplegarMenu() {
+  document.getElementById("myMenu").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("menu-content");
+    var i;
+    for (i = 0; i < dropdowns.length;) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
